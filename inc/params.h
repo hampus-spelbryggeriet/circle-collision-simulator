18398f0d5df7016557a32e21f3f5bea129a65bdc
Hampus Lidin <hampuslidin@gmail.com>
2025-08-28 17:51:22 +0200

    Add build workflow

--- inc/params.h
+++ inc/params.h
@@ -7,7 +7,7 @@ class SimulationParameters {
     int width = 800;
     int height = 450;
     int spawn_limit = 300;
-    float spawn_rate = 1.0;
+    float spawn_rate = 10.0;
     float min_radius = 5.0f;
     float max_radius = 10.0f;
     float simulation_speed = 1.0f;
